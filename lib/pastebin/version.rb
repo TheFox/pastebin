a0c9a0993a6a0b4d1122aff78279d29570faf6c6
Christian Mayer <christian@fox21.at>
2016-10-15 23:16:10 +0200

    GitLab Deployment added.
    Closes #1.

--- lib/pastebin/version.rb
+++ lib/pastebin/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Pastebin
 		NAME = 'Pastebin'
-		VERSION = '1.3.0-dev.2'
-		DATE = '2016-09-12'
+		VERSION = '1.3.0-dev.3'
+		DATE = '2016-10-16'
 		HOMEPAGE = 'https://github.com/TheFox/pastebin'
 	end
 end
